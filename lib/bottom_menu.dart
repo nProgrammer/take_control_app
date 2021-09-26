3708c8497b5abff17b7684ab989e5831e2ea39bd
nProgrammer <wagner.norbert277@outlook.com>
2021-09-26 02:54:22 -0700

    files signed

--- lib/bottom_menu.dart
+++ lib/bottom_menu.dart
@@ -1,3 +1,10 @@
+/*
+  Norbert Wagner (nProgrammer), 2021
+  Take Control App - app wrote in flutter,
+  that is helping people to control
+  usage of stimulants.
+*/
+
 import 'package:flutter/material.dart';
 
 const List<Widget> menuOptions = <Widget>[
