796e5ab6c6df6b97bb2eb39b1cb6843c2f9dbfc9
nProgrammer <wagner.norbert277@gmail.com>
2021-09-28 18:39:26 +0200

    code cleaned up, enabled creating many elements

--- lib/counter_stimulants.dart
+++ lib/counter_stimulants.dart
@@ -30,104 +30,14 @@ Container _generateElement(BuildContext context) {
       ),
       borderRadius: BorderRadius.circular(30),
     ),
-    child: Row(
-      children: [
-        Container(
-          width: xo,
-        ),
-        Image.asset(
-          'beer.png',
-          height: a,
-          width: a,
-        ),
-        Container(
-          width: xo,
-        ),
-        const AutoSizeText(
-          "Beer - 250 ml",
-          style: TextStyle(
-            color: Color.fromARGB(150, 255, 255, 255),
-            fontWeight: FontWeight.bold,
-            fontSize: 50,
-          ),
-          maxLines: 1,
-          minFontSize: 10,
-        ),
-        Container(
-          width: xo,
-        ),
-        Container(
-          width: a,
-          height: a,
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(100, 255, 255, 255),
-            borderRadius: BorderRadius.circular(40),
-          ),
-          child: const AutoSizeText(
-            '10',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Color.fromARGB(150, 255, 255, 255),
-              fontWeight: FontWeight.bold,
-              fontSize: 100,
-            ),
-            maxLines: 1,
-            minFontSize: 10,
-          ),
-        ),
-        Container(
-          width: xo,
-        ),
-        Container(
-          width: a2,
-          height: a2,
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(100, 255, 255, 255),
-            borderRadius: BorderRadius.circular(40),
-          ),
-          child: const AutoSizeText(
-            '-',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Color.fromARGB(150, 255, 255, 255),
-              fontWeight: FontWeight.bold,
-              fontSize: 100,
-            ),
-            maxLines: 1,
-            minFontSize: 10,
-          ),
-        ),
-        Container(
-          width: xp,
-        ),
-        Container(
-          width: a2,
-          height: a2,
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(100, 255, 255, 255),
-            borderRadius: BorderRadius.circular(40),
-          ),
-          child: const AutoSizeText(
-            '+',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Color.fromARGB(150, 255, 255, 255),
-              fontWeight: FontWeight.bold,
-              fontSize: 100,
-            ),
-            maxLines: 1,
-            minFontSize: 10,
-          ),
-        ),
-        Container(
-          width: xo,
-        ),
-      ],
-    ),
+    child: (_generateRowOfElements(xo, a, a2, xp, 'beer.png', 'Beer - 250 ml')),
   );
 }
 
 Row generateCounter(BuildContext context) {
+  double yc = MediaQuery.of(context).size.height;
+  double a3 = 0.1 * yc;
+  double a4 = 0.05 * yc;
   Container _element = _generateElement(context);
   return Row(
     children: [
@@ -139,7 +49,9 @@ Row generateCounter(BuildContext context) {
       ),
       Expanded(
         flex: 70, // 70%
-        child: _element,
+        child: Column(
+          children: _generateChildren(_element, a3, a4),
+        ),
       ),
       Expanded(
         flex: 15, // 15%
@@ -149,4 +61,115 @@ Row generateCounter(BuildContext context) {
       )
     ],
   );
+}
+
+Row _generateRowOfElements(
+    double xo, double a, double a2, double xp, String image, String _desc) {
+  return Row(
+    children: [
+      Container(
+        width: xo,
+      ),
+      Image.asset(
+        image,
+        height: a,
+        width: a,
+      ),
+      Container(
+        width: xo,
+      ),
+      AutoSizeText(
+        _desc,
+        style: const TextStyle(
+          color: Color.fromARGB(150, 255, 255, 255),
+          fontWeight: FontWeight.bold,
+          fontSize: 50,
+        ),
+        maxLines: 1,
+        minFontSize: 10,
+      ),
+      Container(
+        width: xo,
+      ),
+      Container(
+        width: a,
+        height: a,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(100, 255, 255, 255),
+          borderRadius: BorderRadius.circular(40),
+        ),
+        child: const AutoSizeText(
+          '10',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Color.fromARGB(150, 255, 255, 255),
+            fontWeight: FontWeight.bold,
+            fontSize: 100,
+          ),
+          maxLines: 1,
+          minFontSize: 10,
+        ),
+      ),
+      Container(
+        width: xo,
+      ),
+      Container(
+        width: a2,
+        height: a2,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(100, 255, 255, 255),
+          borderRadius: BorderRadius.circular(40),
+        ),
+        child: const AutoSizeText(
+          '-',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Color.fromARGB(150, 255, 255, 255),
+            fontWeight: FontWeight.bold,
+            fontSize: 100,
+          ),
+          maxLines: 1,
+          minFontSize: 10,
+        ),
+      ),
+      Container(
+        width: xp,
+      ),
+      Container(
+        width: a2,
+        height: a2,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(100, 255, 255, 255),
+          borderRadius: BorderRadius.circular(40),
+        ),
+        child: const AutoSizeText(
+          '+',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Color.fromARGB(150, 255, 255, 255),
+            fontWeight: FontWeight.bold,
+            fontSize: 100,
+          ),
+          maxLines: 1,
+          minFontSize: 10,
+        ),
+      ),
+      Container(
+        width: xo,
+      ),
+    ],
+  );
+}
+
+List<Widget> _generateChildren(Container _element, double a3, double a4) {
+  return [
+    Container(
+      height: a3,
+    ),
+    _element,
+    Container(
+      height: a4,
+    ),
+    _element,
+  ];
 }
